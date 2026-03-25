0c4cd75017b971d15314a8e8b071a6f610670c53
thesarfo <ernestsarfo100@gmail.com>
2026-03-25 00:07:43 +0000

    feat: implement account management

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,16 +1,50 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-)
-func main(){
-	
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello, World!"))
-	})
+	"context"
+	"log"
+	"os"
 
-	fmt.Println("Server is up and running!")
-	http.ListenAndServe(":8080", nil)
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
+
+	"github.com/thesarfo/payments-engine/internal/account"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Printf("godotenv: %v (using existing env vars only)", err)
+	}
+
+	ctx := context.Background()
+	connString := os.Getenv("DATABASE_URL")
+	if connString == ""{
+		log.Fatal("database url is required")
+	}
+
+	pool, err := pgxpool.New(ctx, connString)
+	if err != nil {
+		log.Fatalf("pgx pool: %v", err)
+	}
+	defer pool.Close()
+
+	repo := account.NewPostgresAccountRepository(pool)
+
+	created, err := repo.CreateAccount(ctx, account.Account{
+		Name:     "Cash",
+		Type:     account.AccountTypeAsset,
+		Currency: "USD",
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("created %+v", created)
+
+	loaded, err := repo.GetAccountByID(ctx, created.ID)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("loaded %+v", loaded)
+
 
 }	
